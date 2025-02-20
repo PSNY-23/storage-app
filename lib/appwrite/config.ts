b74c0683ab93761544a2c4bc86ac46872089e1fc
pankaj <pankajteceract@gmail.com>
2025-02-20 17:52:33 +0530

    ts:1,10,32

--- lib/appwrite/config.ts
+++ lib/appwrite/config.ts
@@ -4,6 +4,6 @@ export const appwriteConfig = {
   databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!,
   usersCollectionId: process.env.NEXT_PUBLIC_APPWRITE_USERS_COLLECTION!,
   filesCollectionId: process.env.NEXT_PUBLIC_APPWRITE_FILES_COLLECTION!,
-    bucketId: process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID!,
-  secretKey: process.env.NEXT_APPWRITE_KEY!
+  bucketId: process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID!,
+  secretKey: process.env.NEXT_APPWRITE_KEY!,
 };
