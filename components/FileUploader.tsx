1c99e2aa8071a5f2aadfc176b2f32d12e0ccf292
pankaj <pankajteceract@gmail.com>
2025-02-23 18:43:50 +0530

    ts:2:37:51, no bug at thsi moment

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+"use client";
 
-const FileUploader = () => {
-  return (
-    <div>FileUploader</div>
-  )
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { getFileType } from "@/lib/utils";
+import Thumbnail from "./Thumbnail";
+
+interface Props {
+  ownerId: string;
+  accountId: string;
+  className?: string;
 }
 
-export default FileUploader
+interface File {}
+
+const FileUploader = ({ ownerId, accountId, className }: Props) => {
+  const [files, setFiles] = useState<File[]>([]);
+  const onDrop = useCallback(async (acceptedFiles: File[]) => {
+    // Do something with the files
+    setFiles(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()} className='cursor-pointer'>
+      <input {...getInputProps()} />
+      <Button type='button' className={cn("uploader-button", className)}>
+        <Image src='/assets/icons/upload.svg' alt='upload' width={24} height={24} />
+      </Button>
+      {files.length > 0 && (
+        <ul className='uploader-preview-list'>
+          <h4 className='h4 text-light-100'>Uploading</h4>
+          {files.map((file, index) => {
+            const { types, extension } = getFileType(file.name);
+            return (
+              <li key={`${files.name}-${index}`} className='uploader-preview-item'>
+                <div className="flex items-center gap-3">
+                  <Thumbnail type={type} extension={extension} />
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+
+      {isDragActive ? <p>Drop the files here ...</p> : <p>Drag and drop some files here, or click to select files</p>}
+    </div>
+  );
+};
+
+export default FileUploader;
