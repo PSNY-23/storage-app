4143e1791cf1cc800507873a35d7bf856c836cb8
pankaj <pankajteceract@gmail.com>
2025-02-24 09:43:35 +0530

    ts:2:45:08

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -3,7 +3,7 @@
 import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { Button } from "./ui/button";
-import { cn } from "@/lib/utils";
+import { cn, convertFileToUrl } from "@/lib/utils";
 import Image from "next/image";
 import { getFileType } from "@/lib/utils";
 import Thumbnail from "./Thumbnail";
@@ -14,7 +14,7 @@ interface Props {
   className?: string;
 }
 
-interface File {}
+
 
 const FileUploader = ({ ownerId, accountId, className }: Props) => {
   const [files, setFiles] = useState<File[]>([]);
@@ -23,6 +23,14 @@ const FileUploader = ({ ownerId, accountId, className }: Props) => {
     setFiles(acceptedFiles);
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  const handleRemoveFile = (e: React.MouseEvent<HTMLButtonElement,MouseEvent>, fileName: string) => {
+    e.stopPropagation();
+    setFiles((prevFiles) => prevFiles.filter((file) => 
+      file.name !==  fileName
+    ))
+
+  }
 
   return (
     <div {...getRootProps()} className='cursor-pointer'>
@@ -34,12 +42,18 @@ const FileUploader = ({ ownerId, accountId, className }: Props) => {
         <ul className='uploader-preview-list'>
           <h4 className='h4 text-light-100'>Uploading</h4>
           {files.map((file, index) => {
-            const { types, extension } = getFileType(file.name);
+            const { type, extension } = getFileType(file.name);
             return (
-              <li key={`${files.name}-${index}`} className='uploader-preview-item'>
+              <li key={`${file.name}-${index}`} className='uploader-preview-item'>
                 <div className="flex items-center gap-3">
-                  <Thumbnail type={type} extension={extension} />
+                  <Thumbnail type={type} extension={extension} url={convertFileToUrl(file)} />
+                  <div className="preview-item-name">
+                    {file.name}
+                    <Image src="/assets/icons/file-loader.gif" alt="file loader" width={80} height={26}/>
+                  </div>
+
                 </div>
+              <Image src="/assets/icons/remove.svg" alt="remove" height={24} width={24} onClick={(e) => handleRemoveFile(e, file.name)}/>
               </li>
             );
           })}
